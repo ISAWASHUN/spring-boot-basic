558c80d6381fc09a869ea4ff0fb0c735132baee2
ISAWASHUN <syun20000616@icloud.com>
2024-12-07 23:54:52 +0900

    /を追加

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.demo.model.Inquiry;
 import com.example.demo.model.User;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @Controller
 public class HomeController {
@@ -19,6 +19,19 @@ public class HomeController {
 
   @PostMapping("/form")
   private String confirm(@ModelAttribute User user) {
+    return "confirm";
+  }
+
+  @GetMapping("/")
+  public String index(Inquiry inquiry) {
+    return "index";
+  }
+
+  @PostMapping("/")
+  public String confirm(@Validated Inquiry inquiry, BindingResult result) {
+    if(result.hasErrors()) {
+      return "index";
+    }
     return "confirm";
   }
 }
